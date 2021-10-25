2f465aee0f81afdf1a29d7f89cf57d80aae9847d
nbaker00 <89879388+nbaker00@users.noreply.github.com>
2021-10-25 16:16:06 -0500

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,14 +1,17 @@
-# Python code to demonstrate naive method
-# to compute factorial
+from flask import Flask
+import math
+app = Flask(__name__)
+
 n = int(input("Enter a number: "))
 fact = 1
-
-
 for i in range(1,n+1):
     fact = fact * i
-    
 if n >= 0:
     print ("The factorial of the number is: ",end="")
     print(fact)
 else:
     print("Please enter a non negative integer")
+
+@app.route("/factorial")
+def factorial():
+    return ('input: {}'.format(n)) + ('\n\n\n\n\n\n\noutput: {}'.format(fact))
