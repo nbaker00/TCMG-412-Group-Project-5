4ec9a746f0445af98784a3f93d8f89791c044977
lwatson430 <89879383+lwatson430@users.noreply.github.com>
2021-10-25 21:45:38 -0500

    Update fibonacci.py
    Updated Fibonacci code that outputs to the website. Lmk if it works for you!

--- fibonacci.py
+++ fibonacci.py
@@ -1,23 +1,29 @@
-
+from flask import Flask
+app = Flask(__name__)
 number = int(input("Enter a number: "))
+f =[]
+
+a = 0
+b = 1
+c = 0
+if number == 1:
+    f.append(a)
+elif number < 1:
+    print("Please enter a positive integer")
+else:
+    f.append(a)
+    f.append(b)
+    while c < number:
+        c = a + b
+        a = b
+        b = c
+        f.append(c)
+
+print(f)
+        
+
+
 
 @app.route("/fibonacci")
-def fib(number):
-    a = 0
-    b = 1
-    c = 0
-    if number == 1:
-        print(a)
-    elif number < 1:
-        print("Please enter a positive integer")
-    else:
-        print(a)
-        print(b)
-        while c < number:
-            c = a + b
-            a = b
-            b = c
-            print(c)
-
-return fib(number)
-  
+def fib():
+    return "The sequence is {}.".format(str(f)[1:-1])
